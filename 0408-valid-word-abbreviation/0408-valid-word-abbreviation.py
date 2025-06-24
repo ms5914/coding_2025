0262fa80e9de7ecb16d6cd72cf15e2e39a8c4478
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-06-24 12:51:52 -0700

    Time: 0 ms (100%), Space: 12.6 MB (11.67%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -12,6 +12,8 @@ class Solution(object):
                 w+=1
                 a+=1
             elif abbr[a].isdigit():
+                if int(abbr[a]) == 0:
+                    return False
                 s = 0
                 while a<len(abbr) and abbr[a].isdigit():
                     s = s*10+(ord(abbr[a])-ord('0'))
