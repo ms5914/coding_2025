a690ae68ec9298565e7457cf4d675738ea052688
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-06-24 12:48:57 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+        w, a = 0, 0
+
+        while w < len(word) and a < len(abbr):
+            if word[w] == abbr[a]:
+                w+=1
+                a+=1
+            elif abbr[a].isdigit():
+                s = 0
+                while a<len(abbr) and abbr[a].isdigit():
+                    s = s*10+(ord(abbr[a])-ord('0'))
+                    a+=1
+                w+=s
+
+            else:
+                return False
+        return a == len(abbr) and w == len(word)
+    
+                
