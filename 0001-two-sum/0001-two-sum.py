e8b8b2b882a01ebf784ecaf811b8d5b468ced727
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-09-23 05:29:07 -0700

    Time: 4 ms (41.57%), Space: 19.3 MB (5.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,25 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hm = {}
-        for i, num in enumerate(nums):
-            if target-num in hm:
-                return [hm[target-num], i]
-            hm[num] = i
+        # hm = {}
+        # for i, num in enumerate(nums):
+        #     if target-num in hm:
+        #         return [hm[target-num], i]
+        #     hm[num] = i
+
+        nums_with_index = [(num, i) for i, num in enumerate(nums)]
+        nums_with_index.sort()
+        low = 0
+        high = len(nums_with_index)-1
+
+        while low<high:
+            if nums_with_index[low][0]+nums_with_index[high][0] == target:
+                return [nums_with_index[low][1], nums_with_index[high][1]]
+            elif nums_with_index[low][0]+nums_with_index[high][0] < target:
+                low+=1
+            else:
+                high-=1
+            
+
+
+
         
