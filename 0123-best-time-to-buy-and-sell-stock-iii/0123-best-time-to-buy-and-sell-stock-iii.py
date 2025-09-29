cb744d71c4b1addb273a399a21b7d1db6cedc693
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-09-29 10:16:42 -0700

    Time: 302 ms (51.81%), Space: 28.9 MB (54.19%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -11,10 +11,18 @@ class Solution:
         empty = 0
         max_profit = 0
         for i in range(len(prices)):
+            #if i buy 1 at this price
             buy1 = max(buy1, -1*prices[i])
+
+            #if i sell 1 at this price (minus buy1 price)
             sell1 = max(sell1, prices[i]+buy1)
+
+            #if i buy 2 at this price (accumulate previous sell1 profit as well)
             buy2 = max(buy2, sell1-prices[i])
+
+            #if i sell 2 at this price
             sell2 = max(sell2, prices[i]+buy2)
+
             max_profit = max(max_profit, sell1, sell2, empty)
         return max_profit
 
