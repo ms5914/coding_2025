775ed04f59c9003282d1e8f1b197f77708c4e229
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-10-03 21:45:06 -0700

    Time: 0 ms (100%), Space: 17.5 MB (96.62%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        first = nums[0]
+        second = max(first, nums[1])
+        
+        max_profit = second
+        
+        for money in nums[2:]:
+            curr_profit = money+first
+            max_profit = max(curr_profit, max_profit)
+            first = second
+            second = max_profit
+        return max_profit
+            
+        
