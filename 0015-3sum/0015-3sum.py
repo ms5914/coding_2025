793c3ae694c082af145eb9281b7c79ffa7322903
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-10-12 16:17:14 -0700

    Time: 615 ms (34.48%), Space: 20.8 MB (19.96%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,41 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #sort the array
         nums.sort()
-        res = []
-        for i in range(len(nums)):
-            if i>0 and nums[i] == nums[i-1]:
-                continue
-            self.find_two_sum(nums[i], i+1, len(nums)-1, nums, res)
-        return res
         
-    def find_two_sum(self, target, start, end, nums, res):
-        while start<end:
-            if nums[start]+nums[end] == -1*target:
-                res.append([nums[start], nums[end], target])
-                start+=1
-                end-=1
-                while start<end and nums[start] == nums[start-1]:
-                    start+=1
-                while start<end and nums[end] == nums[end+1]:
-                    end-=1
-            elif nums[start]+nums[end] < -1*target:
-                start+=1
-                while start<end and nums[start] == nums[start-1]:
-                    start+=1
-            else:
-                end-=1
-                while start<end and nums[end] == nums[end+1]:
-                    end-=1
-
-
+        if not nums:
+            return []
+        
+        
+        def find_two_sum(j, reqd_sum, nums):
+            k = len(nums)-1
+            result = []
+            
+            while j<k:
+                if nums[j]+nums[k] == reqd_sum:
+                    result.append([nums[j], nums[k]])
+                    j+=1
+                    k-=1
+                    while nums[j] == nums[j-1] and j<k:
+                        j+=1
+                    while nums[k] == nums[k+1] and j<k:
+                        k-=1
+                elif nums[j]+nums[k]<reqd_sum:
+                    j+=1
+                else:
+                    k-=1
                 
+            return result
+            
+            
+            
+        result = []
+        for i, num in enumerate(nums):
+            if i>0 and nums[i-1] == num:
+                continue
+            for res in find_two_sum(i+1, -1*num, nums):
+                res = res + [num]
+                result.append(res)
+            
+        return result
         
-        
-            [-4, -1, -1, 0, 1, 2 ]
+            
