3e1b668a38b2108998d88907ab427175feaabcef
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-10-12 18:58:17 -0700

    Time: 3 ms (98.36%), Space: 18.1 MB (9.81%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,35 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        
+        if n == 1:
+            return "1"
+        
+        def say(s):
+            print(s)
+            prev = s[0]
+            count = 1
+            li = []
+            for ch in s[1:]:
+                if ch == prev:
+                    count+=1
+                else:
+                    li.append(str(count))
+                    li.append(prev)
+                    prev = ch
+                    count = 1
+            li.append(str(count))
+            li.append(prev)
+            return "".join(li)
+        
+        prev_str = "1"
+        for i in range(1, n): 
+            res = say(prev_str)
+            prev_str = res
+        return res
+        
+        
+        
+
+                
+                    
+                
