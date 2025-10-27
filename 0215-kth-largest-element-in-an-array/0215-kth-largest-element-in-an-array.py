9e42eaf6b3f7fcc08dc5a329a6a45fc7a6c94efc
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-10-27 13:28:27 -0700

    Time: 479 ms (5.02%), Space: 28.9 MB (23.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,23 +1,78 @@
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        n = len(nums)
         
-        mapp = Counter(nums)
-        maxim = max(mapp.keys())
-        minim = min(mapp.keys())
-
+        def partition(nums, p, r):
+            index = random.randint(p, r)
+            pivot_ele = nums[index]
+            nums[r], nums[index] = nums[index], nums[r]
+            
+            
+            less_ind = p
+            for ind in range(p, r):
+                on_left = random.randint(0, 1) == 1
+                if nums[ind] < pivot_ele:
+                    nums[ind], nums[less_ind] = nums[less_ind], nums[ind]
+                    less_ind+=1
+                elif nums[ind] == pivot_ele and on_left:
+                    nums[ind], nums[less_ind] = nums[less_ind], nums[ind]
+                    less_ind+=1
+            nums[less_ind], nums[r] = nums[r], nums[less_ind]
+            
+            # print(less_ind)
+            # print(nums)
+            return less_ind
+                
+        
+        def find_k_largest(nums, p, r):
+            if p>r:
+                return
+            pivot = partition(nums, p, r)
+            if pivot == n-k:
+                return nums[n-k]
+            
+            elif pivot< n-k:
+                find_k_largest(nums, pivot+1, r)
+            else:
+                find_k_largest(nums, p, pivot-1)
+            
+            return nums[n-k]
         
         
-        for i in range(maxim, minim-1,-1):
-            if i in mapp:
-                k-=mapp[i]
-                if k<=0:
-                    return i
-        
-
-
+        return find_k_largest(nums, 0, n-1)
     
+    
+    
+    
+#     class Solution {
+#     int quickSelect(int l, int r, int k, vector<int>& nums) {
+#         int randomIdx = rand() % (r - l + 1) + l;
+#         swap(nums[randomIdx], nums[r]);
+#         int p = l;
+#         for(int i = l; i < r; ++i) {
+#             int couldBeSame = rand() % 2;
+#             if((couldBeSame && nums[i] <= nums[r]) ||
+#                (!couldBeSame && nums[i] < nums[r])) {
+#                 swap(nums[p], nums[i]);
+#                 ++p;
+#             }
+#         }
+#         swap(nums[p], nums[r]);
+#         if(p == k) {
+#             return nums[p];
+#         } else if(p < k) {
+#             return quickSelect(p + 1, r, k, nums);
+#         } else {
+#             return quickSelect(l, p - 1, k, nums);
+#         }
+#     }
+# public:
+#     int findKthLargest(vector<int>& nums, int k) {
+#         k = nums.size() - k;
+#         return quickSelect(0, nums.size() - 1, k, nums);
+#     }
+# };
+            
+        
+            
+        
