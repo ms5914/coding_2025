a0821eee567d89c23a019403026999dd17634fa5
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-10-26 22:07:44 -0700

    Time: 3 ms (90.58%), Space: 21.4 MB (28.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,59 +1,44 @@
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        unique = list(count.keys())
-        
-        def partition(left, right, pivot_index) -> int:
-            pivot_frequency = count[unique[pivot_index]]
-            # 1. Move the pivot to end
-            unique[pivot_index], unique[right] = unique[right], unique[pivot_index]  
-            
-            # 2. Move all less frequent elements to the left
-            store_index = left
-            for i in range(left, right):
-                if count[unique[i]] < pivot_frequency:
-                    unique[store_index], unique[i] = unique[i], unique[store_index]
-                    store_index += 1
+        count_map = Counter(nums)
+        unique = list(count_map.keys())
+        n = len(nums)
+        unique_n = len(unique)
 
-            # 3. Move the pivot to its final place
-            unique[right], unique[store_index] = unique[store_index], unique[right]  
-            
-            return store_index
-        
-        def quickselect(left, right, k_smallest) -> None:
-            """
-            Sort a list within left..right till kth less frequent element
-            takes its place. 
-            """
-            # base case: the list contains only one element
-            if left == right: 
+        def partition(left, right):
+            if left>=right:
                 return
+            index = random.randint(left, right)
+            index_freq = count_map[unique[index]]
+            unique[index], unique[right] = unique[right], unique[index]
 
-            print(left, right)
-            
-            # Select a random pivot_index
-            pivot_index = random.randint(left, right)     
-                            
-            # Find the pivot position in a sorted list   
-            pivot_index = partition(left, right, pivot_index)
-            
-            # If the pivot is in its final sorted position
-            if k_smallest == pivot_index:
-                 return 
-            # go left
-            elif k_smallest < pivot_index:
-                quickselect(left, pivot_index - 1, k_smallest)
-            # go right
+            left_index = left
+            for i in range(left, right):
+                if count_map[unique[i]]< index_freq:
+                    unique[left_index], unique[i] = unique[i], unique[left_index]
+                    left_index+=1
+            unique[left_index], unique[right] = unique[right], unique[left_index]
+            return left_index
+        
+        def find_k_frequent(left, right):
+            if left >= right:
+                return
+            piv_index = partition(left, right)
+            if index == unique_n-k:
+                return
+            elif piv_index<unique_n-k:
+                find_k_frequent(piv_index+1, right)
             else:
-                quickselect(pivot_index + 1, right, k_smallest)
-         
-        n = len(unique) 
-        # kth top frequent element is (n - k)th less frequent.
-        # Do a partial sort: from less frequent to the most frequent, till
-        # (n - k)th less frequent element takes its place (n - k) in a sorted array. 
-        # All elements on the left are less frequent.
-        # All the elements on the right are more frequent.  
-        quickselect(0, n - 1, n - k)
-        # Return top k frequent elements
-        return unique[n - k:]
+                find_k_frequent(left, piv_index-1)
+        
+        find_k_frequent(0, unique_n-1)
+        return unique[unique_n-k:]
+        
+        
+
+
+
+
+
+
+
