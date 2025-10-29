7a8d2e834dd32820a92e8871f73305144544e6c7
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-10-29 15:07:22 -0700

    Time: 175 ms (5.13%), Space: 35.9 MB (96.67%) - LeetHub

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        window_size = 0
+        max_free = 0
+        n = len(startTime)
+        occupied, free_time = 0, 0
+        for i in range(n):
+            window_size+=1
+            free_time += startTime[i]-endTime[i-1] if i>0 else startTime[i]
+            if window_size == k:
+                toadd = startTime[i+1]-endTime[i] if i<n-1 else eventTime-endTime[i]
+                print(free_time, toadd)
+                total_time = free_time+toadd
+                print(total_time)
+                max_free = max(max_free, total_time)
+                window_size -=1
+                free_time -= startTime[i-k+1]-endTime[i-k] if i-k+1>0 else startTime[i-k+1]
+        return max_free
+
+            
+        
