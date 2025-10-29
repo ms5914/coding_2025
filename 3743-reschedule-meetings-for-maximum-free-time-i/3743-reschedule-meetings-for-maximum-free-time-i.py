46199be7b80e03520ea4b1794fc8043227b06add
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-10-29 15:13:11 -0700

    Time: 60 ms (58.97%), Space: 36 MB (77.69%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.py
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.py
@@ -1,6 +1,10 @@
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
-     
+             #The idea is that 1. the k events that we should move should always be contiguous, and so this 
+        # becomes a sliding window problem where we assume that we will compress together the contiguous k events and then find out the empty space  
+        # after every k size window(the empty space will be the summation of space before first event + space between first and second event + space between second and third and so + space between kth and k-th event and in the space between kth and kth+1 event (this will be calculated via toadd variable))
+        #then just slide the window, i.e decrement the window size by 1, remove the i-kth space that we had previously added
+        #the answer is the max_free time that we can get via all such windows  
         
         window_size = 0
         max_free = 0
