36eddf67e630b9835e80e4ab1318d1cfdc01ea85
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-04 15:27:02 -0800

    Time: 0 ms (100%), Space: 17.7 MB (79.03%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
+
+        #this is base 26 conversion to base 10. 
         columnNumber = 0
         for ch in columnTitle:
             columnNumber = columnNumber*26+(ord(ch)-ord("A")+1)
