72c93144cfd3e6baac3fd12b925b2825518fa977
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-10 15:18:10 -0800

    Time: 3 ms (19.77%), Space: 17.9 MB (42.71%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,52 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        
+
+          # Constants.
+        MAX_INT = 2147483647  # 2**31 - 1
+        MIN_INT = -2147483648  # -2**31
+        HALF_MIN_INT = -1073741824  # MIN_INT // 2
+
+        # Special case: overflow.
+        if dividend == MIN_INT and divisor == -1:
+            return MAX_INT
+            
+        num_negatives = 0
+        if dividend < 0:
+            num_negatives+=1
+        if divisor < 0:
+            num_negatives+=1
+        
+        dividend = -dividend if dividend > 0 else dividend
+        divisor = -divisor if divisor > 0 else divisor
+        
+        key = divisor
+        count = 0
+        while dividend<=key:
+            print("outer", dividend, key)
+            times = 1
+            while dividend <= key+key and key>pow(-2, 31)//2:
+                print(dividend, key)
+                times+=times
+                key += key
+            dividend -= key
+            key = divisor
+            count += times
+        
+        if num_negatives == 1:
+            return -count
+        else:
+            return count
+        
+        
+        
+        
+        
+        
+        
+            
+            
+            
+            
+        
+        
