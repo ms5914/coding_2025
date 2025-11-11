cd066f43675160784c0933419c970c39b5c14fc6
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-11 12:32:06 -0800

    Time: 0 ms (100%), Space: 18 MB (8.07%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -4,13 +4,26 @@ class Solution:
         y = abs(b)
         if y>x:
             return self.getSum(b, a)
+
+
+        #sign of larger number    
         sign = 1 if a>=0 else -1
         
+
+        #if addition, keep adding number and carry (adding carry is also addition)
+        #keep on adding until carry becomes zero
+        #xor is addition without carry
+        # left shift by 1 of x&y is the carry 
         if a*b >= 0:
             while y:
                 x, y = x^y, (x&y) <<1
 
         else:
+
+        #if subtracting, keep subtracting number and borrow (subtracting borrow is also subtract)
+        #keep on subtracting until borrow becomes zero
+        #xor is subtraction without carry also
+        # left shift by 1 of ~x&y is the borrow (~x&y finds positions k where x[k] is 0 and y[k] is 1 so that borrow is needed) 
             while y:
                 x, y = x^y, ((~x)&y) <<1
         
