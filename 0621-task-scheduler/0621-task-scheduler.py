27de7b34c046d0716f24bb6c41003a3f3c6a1e85
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-11 17:30:00 -0800

    Time: 33 ms (79.1%), Space: 18.9 MB (98.41%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,7 +1,11 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count_map = defaultdict(int)
+        
+        #max count
         max_count = 0
+
+        #no of letters with max count
         max_count_letter_count = 0
         
         for task in tasks:
@@ -13,11 +17,16 @@ class Solution:
                 max_count_letter_count+=1
         
         empty_slots = n * (max_count-1)
+        
+        # For max frequency elements only max_freq-1 elements can fill empty spots, 1 of each of     those  will be added towards the end. Empty slots can't be negative, because rest of the elements can be plugged in between. 
+
         empty_slots_updated = max(0, empty_slots - ((max_count-1)*(max_count_letter_count-1)))
+        
+        #max freq elements are already taken into account via empty_slots_updated
         tasks_yet_to_be_assigned = len(tasks)- (max_count*max_count_letter_count)
+
+        #final empty slots
         empty_slots = max(0, empty_slots_updated-tasks_yet_to_be_assigned)
 
         return len(tasks)+empty_slots
-            
-            
         
