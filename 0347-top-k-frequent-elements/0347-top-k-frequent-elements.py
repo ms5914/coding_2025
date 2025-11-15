53151e5c61ffdad580f1e3eee782bf422f3757fb
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-15 14:38:50 -0800

    Time: 8 ms (32.2%), Space: 21.4 MB (39.14%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,43 +1,45 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count_map = Counter(nums)
-        unique = list(count_map.keys())
-        n = len(nums)
-        unique_n = len(unique)
+        unique_elements = list(count_map.keys())
+        n = len(unique_elements)
+        print(count_map)
+        print(unique_elements)
 
-        def partition(left, right):
-            if left>right:
+        def partition(A, p, r):
+            if p>r:
                 return
-            index = random.randint(left, right)
-            index_freq = count_map[unique[index]]
-            unique[index], unique[right] = unique[right], unique[index]
+            
+            ind = random.randint(p,r)
+            print(ind)
+            freq = count_map[A[ind]]
+            A[ind], A[r] = A[r], A[ind]
 
-            left_index = left
-            for i in range(left, right):
-                if count_map[unique[i]]< index_freq:
-                    unique[left_index], unique[i] = unique[i], unique[left_index]
-                    left_index+=1
-            unique[left_index], unique[right] = unique[right], unique[left_index]
-            return left_index
+            k = p
+            for i in range(p, r):
+                if count_map[A[i]] < freq:
+                    A[k], A[i] = A[i], A[k]
+                    k+=1
+            A[k], A[r] = A[r], A[k]
+            
+            return k
         
-        def find_k_frequent(left, right):
-            if left > right:
+
+        def find_top_k(A, p, r):
+            if p>=r:
                 return
-            piv_index = partition(left, right)
-            if index == unique_n-k:
+            ind = partition(A, p, r)
+
+            if ind == n-k:
                 return
-            elif piv_index<unique_n-k:
-                find_k_frequent(piv_index+1, right)
+            elif ind < n-k:
+                find_top_k(A, ind+1, r)
             else:
-                find_k_frequent(left, piv_index-1)
+                find_top_k(A, p, ind-1)
         
-        find_k_frequent(0, unique_n-1)
-        return unique[unique_n-k:]
-        
-        
-
-
-
+        find_top_k(unique_elements, 0, n-1)
+        return unique_elements[n-k:]
+                
 
 
 
