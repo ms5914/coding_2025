3d4ca05963e11e3b1575462da923df993082c6a6
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-17 14:15:52 -0800

    Time: 0 ms (100%), Space: 17.9 MB (17.59%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        count_letters = Counter(s)
+
+        result_str = []
+        for ch in order:
+            if ch in count_letters:
+                for i in range(count_letters[ch]):   
+                    result_str.append(ch)
+                del count_letters[ch]
+        
+        for ch in count_letters:
+            for i in range(count_letters[ch]):
+                result_str.append(ch)
+        
+        return "".join(result_str)
+
+
+
+        
