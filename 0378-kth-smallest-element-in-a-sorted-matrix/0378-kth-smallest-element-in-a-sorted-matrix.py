5e1f1883f3acc77f7b2121bf68bcbd06779770c9
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-19 23:04:08 -0800

    Time: 27 ms (43.22%), Space: 21.1 MB (85.73%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,36 +1,51 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        def find_smaller_and_equal(mid):
-            i = len(matrix)-1
-            j = 0
-            count_smaller = 0
-            smaller = matrix[0][0]
-            larger = matrix[len(matrix)-1][len(matrix[0])-1]
+        min_heap = []
+        n = len(matrix)
 
-            while i>=0 and j<len(matrix[0]):
-                if matrix[i][j] <= mid:
-                    count_smaller+=i+1
-                    smaller = max(smaller, matrix[i][j])
-                    j+=1
-                else:
-                    larger = min(larger, matrix[i][j])
-                    i-=1
-            return count_smaller, smaller, larger
+        for j in range(n):
+            min_heap.append((matrix[0][j], 0, j))
+        
+        heapq.heapify(min_heap)
 
-        low = matrix[0][0]
-        high = matrix[len(matrix)-1][len(matrix[0])-1]
+        while k>0:
+            ele, row, col = heapq.heappop(min_heap)
+            if row<n-1:
+                heapq.heappush(min_heap, (matrix[row+1][col], row+1, col))
+            k-=1
+        return ele
+        # def find_smaller_and_equal(mid):
+        #     i = len(matrix)-1
+        #     j = 0
+        #     count_smaller = 0
+        #     smaller = matrix[0][0]
+        #     larger = matrix[len(matrix)-1][len(matrix[0])-1]
+
+        #     while i>=0 and j<len(matrix[0]):
+        #         if matrix[i][j] <= mid:
+        #             count_smaller+=i+1
+        #             smaller = max(smaller, matrix[i][j])
+        #             j+=1
+        #         else:
+        #             larger = min(larger, matrix[i][j])
+        #             i-=1
+        #     return count_smaller, smaller, larger
+
+        # low = matrix[0][0]
+        # high = matrix[len(matrix)-1][len(matrix[0])-1]
         
-        while low<high:
-            mid = low+(high-low)//2
-            count, smaller, larger = find_smaller_and_equal(mid)
-            if count == k:
-                return smaller
-            elif count>k:
-                high = smaller
-            else:
-                low = larger
+        # while low<high:
+        #     mid = low+(high-low)//2
+        #     count, smaller, larger = find_smaller_and_equal(mid)
+        #     if count == k:
+        #         return smaller
+        #     elif count>k:
+        #         high = smaller
+        #     else:
+        #         low = larger
         
-        return low
+        # return low
+
 
 
 
