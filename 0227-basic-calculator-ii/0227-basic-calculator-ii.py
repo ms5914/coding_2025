55268d6ca3f01f3208b2586c7a5335c752bb039d
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-20 16:33:54 -0800

    Time: 157 ms (6.61%), Space: 19.2 MB (73.06%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -6,6 +6,8 @@ class Solution:
         prev_sign = "+"
         result = 0
         i = 0
+
+        #prev_num is the last expression evaluated (so if the prev sign is +/-, just add that last evaluation to result and prepare new evaluation)
         for ch in s:
             print(ch)
             if ch.isdigit():
