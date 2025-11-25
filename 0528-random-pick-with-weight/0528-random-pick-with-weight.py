4704a0c1d38d19b881c875d93f3bd81785fe6009
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-25 00:02:48 -0800

    Time: 66 ms (66.83%), Space: 23.1 MB (91.36%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,29 +1,26 @@
-import random
-from bisect import bisect_left
-
-class Solution(object):
-
-    def __init__(self, w):
+class Solution:
+    def __init__(self, w: List[int]):
         """
         :type w: List[int]
         """
-        s = 0
-        self.array = []
-        for elem in w:
-            s+=elem
-            self.array.append(s)
+        self.prefix_sums = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sums.append(prefix_sum)
+        self.total_sum = prefix_sum
 
-    def pickIndex(self):
+    def pickIndex(self) -> int:
         """
         :rtype: int
         """
-        return bisect.bisect_left(self.array, random.randint(1, self.array[-1]))
-
-
-
-        
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(w)
-# param_1 = obj.pickIndex()
+        target = self.total_sum * random.random()
+        # run a binary search to find the target zone
+        low, high = 0, len(self.prefix_sums)-1
+        while low < high:
+            mid = low + (high - low) // 2
+            if target > self.prefix_sums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
