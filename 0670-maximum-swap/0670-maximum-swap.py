622ff8186cc14fc2d66984d0d798d69d13b79fbd
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-25 13:25:13 -0800

    Time: 0 ms (100%), Space: 17.7 MB (83.84%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,27 +1,25 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        nums = list(str(num))
-        
-        left = -1
-        right = -1
 
-        max_so_far = len(nums)-1
-        for i in range(len(nums)-2, -1, -1):
-            if nums[max_so_far] > nums[i]:
+        num_str = list(str(num))
+        left, right = -1, -1
+        max_so_far = num_str[-1]
+        max_so_far_index = len(num_str)-1
+        
+        for i in range(len(num_str)-2, -1, -1):
+            if num_str[i]>max_so_far:
+                max_so_far = num_str[i]
+                max_so_far_index = i
+            elif num_str[i]<max_so_far:
                 left = i
-                right = max_so_far
-            elif nums[i] > nums[max_so_far]:
-                max_so_far = i
-        if left != -1 and right != -1:
-            temp = nums[left]
-            nums[left] = nums[right]
-            nums[right] = temp
-            return int("".join(nums))
+                right = max_so_far_index
+        
+        if left !=-1 and right!=-1:
+            temp = num_str[left]
+            num_str[left] = num_str[right]
+            num_str[right] = temp
+            return int("".join(num_str))
+        
         else:
             return num
-
-
-
-
-
-        
+            
