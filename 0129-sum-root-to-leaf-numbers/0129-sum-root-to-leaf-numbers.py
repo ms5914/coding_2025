523a92d9d152a012e99758ea3b87cb430bdff16b
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-24 16:44:08 -0800

    Time: 0 ms (100%), Space: 17.8 MB (36.22%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,20 +6,42 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        result = 0
-       
-        def find_number(root, number):
-            nonlocal result
-            if not root:
-                return
-            number = number*10+root.val
-            if not root.left and not root.right:
-                result+=number
+      #preorder traversal
+
+      result = 0
+      curr_num = 0
+      
+      while root:
+        if not root.left:
+            curr_num = curr_num*10+root.val
+            if root.right is None:
+                result += curr_num
+            root = root.right
+        else:
+            root_left = root.left
+            step = 1
+            while root_left.right and root_left.right != root:
+                root_left = root_left.right
+                step+=1
+            
+            if root_left.right == root:
+                root_left.right = None
+                if not root_left.left:
+                    result+=curr_num
+                root = root.right
+                for i in range(step):
+                    curr_num //= 10
+                
             else:
-                find_number(root.left, number)
-                find_number(root.right, number)
+                root_left.right = root
+                curr_num = curr_num*10+root.val
+                root = root.left
+                
+      return result
         
-        find_number(root, 0)
-        return result
+
+
+
+
             
             
