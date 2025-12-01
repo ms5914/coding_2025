b429c8248339070a9df7570b7b882f50deeaba12
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-01 11:48:43 -0800

    Time: 163 ms (14.92%), Space: 28.2 MB (12.99%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -22,3 +22,53 @@ class Solution:
                 dp[i][r] = max(exclude_num, include_num)
                 
         return dp[len(nums)][0]
+
+# class Solution:
+#     def maxSumDivThree(self, nums: List[int]) -> int:
+#         dp = [[0  for j in range(3)] for i in range(len(nums)+1)]
+#         for i in range(1, len(nums)+1):
+#             dp[i] = dp[i-1][:]
+#             for j in dp[i-1]:
+#                 dp[i][(j+nums[i-1])%3] = max(dp[i][(j+nums[i-1])%3], j+nums[i-1])
+#         return dp[len(nums)][0]
+
+
+# def maxSumDivThree(self, nums: List[int]) -> int:
+# 	n = len(nums)
+# 	dp = [[0]*3 for _ in range(n+1)]
+# 	dp[0][1] = float('-inf')
+# 	dp[0][2] = float('-inf')
+# 	for i in range(1, n+1):
+# 		if nums[i-1] % 3 == 0: # Current remainder == 0
+# 			dp[i][0] = max(dp[i-1][0], dp[i-1][0] + nums[i-1]) # Current remainder is 0, so we add to dp[i-1][0] to keep the remainder 0
+# 			dp[i][1] = max(dp[i-1][1], dp[i-1][1] + nums[i-1]) # Current remainder is 0, so we add to dp[i-1][1] to keep the remainder 1
+# 			dp[i][2] = max(dp[i-1][2], dp[i-1][2] + nums[i-1]) # Current remainder is 0, so we add to dp[i-1][2] to keep the remainder 2
+# 		elif nums[i-1] % 3 == 1: # Current remainder == 1
+# 			dp[i][0] = max(dp[i-1][0], dp[i-1][2] + nums[i-1]) # Current remainder is 1, so we add to dp[i-1][2] to keep the remainder 0
+# 			dp[i][1] = max(dp[i-1][1], dp[i-1][0] + nums[i-1]) # Current remainder is 1, so we add to dp[i-1][0] to keep the remainder 1
+# 			dp[i][2] = max(dp[i-1][2], dp[i-1][1] + nums[i-1]) # Current remainder is 1, so we add to dp[i-1][1] to keep the remainder 2
+# 		else: # Current remainder == 2
+# 			dp[i][0] = max(dp[i-1][0], dp[i-1][1] + nums[i-1]) # Can you tell how this works now?
+# 			dp[i][1] = max(dp[i-1][1], dp[i-1][2] + nums[i-1])
+# 			dp[i][2] = max(dp[i-1][2], dp[i-1][0] + nums[i-1])
+
+# 	return dp[-1][0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
