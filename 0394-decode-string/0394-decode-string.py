216f99e5eb786cfb47c61ce0d4e23a2cc4dfeacf
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-30 21:01:39 -0800

    Time: 0 ms (100%), Space: 17.9 MB (33.25%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,34 +1,64 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-
-
-        st = []
-        i = 0
-        while i < len(s):
-            if s[i] == "]":
-                inside_list = []
-                while st and st[-1] != "[":
-                    ch = st.pop()
-                    inside_list.append(ch)
-                st.pop()
-                inside_str = "".join(inside_list[::-1])
-                
-                number_list = []
-                while st and st[-1].isdigit():
-                    number_list.append(st.pop())
-                
-                number = int("".join(number_list[::-1]))
-
-                to_push_list = []
-                for k in range(number):
-                    to_push_list.append(inside_str)
-                
-                st.append("".join(to_push_list))
-            else:
-                st.append(s[i])
-            i+=1
         
-        return "".join(st)
+
+        i = 0
+        def decode(s):
+            result = []
+            nonlocal i
+            while i < len(s) and s[i] !="]":
+                if i<len(s) and not s[i].isdigit():
+                    result.append(s[i])
+                    i+=1
+                else:
+                    num = 0
+                    while i<len(s) and s[i].isdigit():
+                        num = num*10+int(s[i])
+                        i+=1
+                    i+=1
+                    
+                    result_str = decode(s)
+
+                    i+=1
+
+                    for k in range(num):
+                        result.append(result_str)
+                
+            return "".join(result)
+
+        return decode(s)
+                
+
+
+
+
+        # st = []
+        # i = 0
+        # while i < len(s):
+        #     if s[i] == "]":
+        #         inside_list = []
+        #         while st and st[-1] != "[":
+        #             ch = st.pop()
+        #             inside_list.append(ch)
+        #         st.pop()
+        #         inside_str = "".join(inside_list[::-1])
+                
+        #         number_list = []
+        #         while st and st[-1].isdigit():
+        #             number_list.append(st.pop())
+                
+        #         number = int("".join(number_list[::-1]))
+
+        #         to_push_list = []
+        #         for k in range(number):
+        #             to_push_list.append(inside_str)
+                
+        #         st.append("".join(to_push_list))
+        #     else:
+        #         st.append(s[i])
+        #     i+=1
+        
+        # return "".join(st)
         
 
 
