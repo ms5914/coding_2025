d49a603c12c522681474c75142c749bebb2db0a5
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-11-30 18:13:05 -0800

    Time: 77 ms (61.22%), Space: 20.4 MB (25.09%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
@@ -3,27 +3,26 @@ class TicTacToe:
     def __init__(self, n: int):
         self.rows = [0 for i in range(n)]
         self.cols = [0 for i in range(n)]
-        self.diagonal = 0
-        self.antidiagonal = 0
+        self.left_diag = 0
+        self.right_diag = 0
         self.n = n
         
 
     def move(self, row: int, col: int, player: int) -> int:
-        
-        to_add = 1 if player == 1 else -1
-        
-        self.rows[row]+=to_add
-        self.cols[col]+=to_add
-        
+        player = 1 if player == 1 else -1
+        self.rows[row]+=player
+        self.cols[col]+=player
+
         if row == col:
-            self.diagonal+=to_add
+            self.left_diag+=player
         if row+col == self.n-1:
-            self.antidiagonal+=to_add
+            self.right_diag+=player
         
-        if self.rows[row] == to_add*self.n or self.cols[col] == to_add*(self.n) or self.diagonal == to_add*self.n or self.antidiagonal == to_add*self.n:
-            return player
-        else:
-            return 0
+        if player*self.n in (self.rows[row], self.cols[col], self.left_diag, self.right_diag):
+            return 1 if player == 1 else 2
+        
+        return 0
+        
 
 
 # Your TicTacToe object will be instantiated and called as such:
