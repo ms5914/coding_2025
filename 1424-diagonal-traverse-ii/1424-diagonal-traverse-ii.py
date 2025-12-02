87af0d8b58b78e9403be9f75bdd61468f31f4be8
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-02 15:52:32 -0800

    Time: 51 ms (89.99%), Space: 32 MB (77.3%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -1,25 +1,43 @@
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
 
-        min_diagonal = float("inf")
-        max_diagonal = -float("inf")
-
-        diagonal_dict = defaultdict(list)
-        for row in range(len(nums)-1, -1, -1):
-            for col in range(len(nums[row])):
-                diagonal = row+col
-                min_diagonal = min(diagonal, min_diagonal)
-                max_diagonal = max(diagonal, max_diagonal)
-                diagonal_dict[diagonal].append(nums[row][col])
-        
-
         ans = []
-        for diag in range(min_diagonal, max_diagonal+1, 1):
-            if diag in diagonal_dict:
-                for ele in diagonal_dict[diag]:
-                    ans.append(ele)
+        dq = deque()
+        dq.append((0, 0))
+        ans.append(nums[0][0])
+        while dq:
+            row, col = dq.popleft()
+            if col == 0 and row+1<len(nums):
+                ans.append(nums[row+1][col])
+                dq.append((row+1, col))
+            if col+1 < len(nums[row]):
+                dq.append((row, col+1))
+                ans.append(nums[row][col+1])
         
         return ans
+            
+            
+            
+
+        # min_diagonal = float("inf")
+        # max_diagonal = -float("inf")
+
+        # diagonal_dict = defaultdict(list)
+        # for row in range(len(nums)-1, -1, -1):
+        #     for col in range(len(nums[row])):
+        #         diagonal = row+col
+        #         min_diagonal = min(diagonal, min_diagonal)
+        #         max_diagonal = max(diagonal, max_diagonal)
+        #         diagonal_dict[diagonal].append(nums[row][col])
+        
+
+        # ans = []
+        # for diag in range(min_diagonal, max_diagonal+1, 1):
+        #     if diag in diagonal_dict:
+        #         for ele in diagonal_dict[diag]:
+        #             ans.append(ele)
+        
+        # return ans
 
 
                 
