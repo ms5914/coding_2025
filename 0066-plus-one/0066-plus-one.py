176797a09defc7c3b8450b53940dce06b50ffa15
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-01 16:47:22 -0800

    Time: 0 ms (100%), Space: 17.7 MB (79.08%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,41 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # carry = 1
-        # result = []
-        # for num in digits[: : -1]:
-        #     new_num = num+carry
-        #     carry = new_num//10
-        #     new_num = new_num%10
-        #     result.append(new_num)
         
-        # if carry:
-        #     result.append(1)
-        # return result[::-1]
-
         n = len(digits)
-
-        # move along the input array starting from the end
-        for i in range(n):
-            idx = n - 1 - i
-
-            # Set all the nines at the end of the array to zeros
-            if digits[idx] == 9:
-                digits[idx] = 0
-
-            # Here we have the rightmost not-nine
+        for idx, num in enumerate(digits[::-1]):
+            if num == 9:
+                digits[n-idx-1] = 0
             else:
-                # Increase this rightmost not-nine by 1
-                digits[idx] += 1
-
-                # and the job is done
+                digits[n-idx-1]+=1
                 return digits
-
-        # We're here because all the digits are nines
-        return [1] + digits
-
         
+        return [1]+digits
 
-
-            
-            
         
