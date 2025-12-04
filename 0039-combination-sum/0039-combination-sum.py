c3b30bcaa4e30a0ee5fbae5afe62b179f5b53ba6
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-04 14:54:37 -0800

    Time: 7 ms (81.92%), Space: 17.7 MB (87.12%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,27 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum(
+        self, candidates: List[int], target: int
+    ) -> List[List[int]]:
 
-        result = []
-        def find_combinations(i, combination, sum_so_far):
-            if sum_so_far == target:
-                result.append(list(combination))
-            if i>=len(candidates) or sum_so_far >= target:
+        results = []
+
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                # make a deep copy of the current combination
+                results.append(list(comb))
                 return
-            
-            combination.append(candidates[i])
-            find_combinations(i, combination, sum_so_far+candidates[i])
-            combination.pop()
-            find_combinations(i+1, combination, sum_so_far)
+            elif remain < 0:
+                # exceed the scope, stop exploration.
+                return
 
-        find_combinations(0, [], 0)
-        return result
+            for i in range(start, len(candidates)):
+                # add the number into the combination
+                comb.append(candidates[i])
+                # give the current number another chance, rather than moving on
+                backtrack(remain - candidates[i], comb, i)
+                # backtrack, remove the number from the combination
+                comb.pop()
+
+        backtrack(target, [], 0)
+
+        return results
