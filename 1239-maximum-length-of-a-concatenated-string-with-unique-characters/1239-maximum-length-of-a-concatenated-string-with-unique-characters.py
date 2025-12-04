572154af1816dd64b0509dbfda3e491bbb8f1b15
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-03 17:02:07 -0800

    Time: 39 ms (64.34%), Space: 17.9 MB (66.55%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        result = []
+        max_count = 0
+        char_set = set()
+
+
+        def find_max(i, char_set, char_count, indexes):  
+            nonlocal max_count     
+            if char_count>max_count:
+                max_count = char_count
+                result = list(indexes)
+
+            if i>=len(arr):
+                return
+            
+            found = False
+            word_set = set()
+            for ch in arr[i]:
+                if ch in char_set or ch in word_set:
+                    found = True
+                    break
+                word_set.add(ch)
+            if not found:
+                for ch in arr[i]:
+                    char_set.add(ch)
+                indexes.add(i)
+                find_max(i+1,char_set, char_count+len(arr[i]), indexes)
+                for ch in arr[i]:
+                    if ch in char_set:
+                        char_set.remove(ch)
+                indexes.remove(i)
+            find_max(i+1, char_set, char_count, indexes)
+        
+        find_max(0, set(), 0, set())
+        return max_count
+            
+
+            
+
+
+        
