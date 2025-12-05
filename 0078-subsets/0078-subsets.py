0bc11b1c08016c91a0eae4ac17e5f22aff3b4948
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-04 22:15:53 -0800

    Time: 0 ms (100%), Space: 18.1 MB (20.76%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,20 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-        
         result = []
-        
-        def find_subsets(index, current_subset):
-            if index == len(nums):
-                result.append(list(current_subset))
-            else:
-                current_subset.append(nums[index])
-                find_subsets(index+1, current_subset)
-                current_subset.pop()
-                find_subsets(index+1, current_subset)
-        
+        def find_subsets(start, subset):
+            result.append(list(subset))
+            if start == len(nums):
+                return
+            for i in range(start, len(nums)):
+                subset.append(nums[i])
+                find_subsets(i+1, subset)
+                subset.pop()
         find_subsets(0, [])
         return result
-        
             
+
         
