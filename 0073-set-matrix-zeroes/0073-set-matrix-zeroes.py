b94efa1c3654cdec11a53153d215a72aa46c75be
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-04 16:08:07 -0800

    Time: 0 ms (100%), Space: 18.3 MB (95.22%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,37 +3,49 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m, n = len(matrix), len(matrix[0])
-        is_row = False
-        is_col = False
-        
+
+        m = len(matrix)
+        n = len(matrix[0])
+        is_col_zero = False
+
         for i in range(m):
-            for j in range(n):
+            if matrix[i][0] == 0:
+                is_col_zero = True
+        
+        for i in range(n):
+            if matrix[0][i] == 0:
+                matrix[0][0] = 0
+        
+        for i in range(1, m):
+            for j in range(1, n):
                 if matrix[i][j] == 0:
-                    matrix[0][j] = 0
                     matrix[i][0] = 0
-                    if i == 0: 
-                        is_row = True
-                    if j == 0:
-                        is_col = True
+                    matrix[0][j] = 0
+        
+        for i in range(1, m):
+            if matrix[i][0] == 0:
+                for k in range(n):
+                    matrix[i][k] = 0
+        
+        for i in range(1, n):
+            if matrix[0][i] == 0:
+                for k in range(m):
+                    matrix[k][i] = 0
+        
+        if matrix[0][0] == 0:
+            for i in range(0, n):
+                matrix[0][i] = 0
+        
+        if is_col_zero:
+            for i in range(0, m):
+                matrix[i][0] = 0
+        
+        return matrix
+
+
+        
+
+
+
         
         
-        for r in range(1,n):
-            if matrix[0][r] == 0:
-                for s in range(m):
-                    matrix[s][r] = 0
-                    
-        for k in range(1,m):
-            if matrix[k][0] == 0:
-                for l in range(n):
-                    matrix[k][l] = 0
-        if is_row:
-            for p in range(n):
-                matrix[0][p] = 0
-        if is_col:
-            for q in range(m):
-                matrix[q][0] = 0
-        
-            
-                    
-                
