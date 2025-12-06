406b077f6e481997f9a9babe3dd9da569fccb086
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-05 23:01:23 -0800

    Time: 157 ms (78.98%), Space: 19.7 MB (69.97%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -1,20 +1,27 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
-
-        
         def longestPalindromeSubseq(s: str) -> bool:
             n = len(s)
             dp = [[0] * n for _ in range(n)]
 
-            for i in range(n - 1, -1, -1):
+            for i in range(0, n):
                 dp[i][i] = 1
-                for j in range(i + 1, n):
+            
+            for i in range(0, n-1):
+                j = i+1
+                if s[i] == s[j]:
+                    dp[i][j] = 2
+                else:
+                    dp[i][j] = 1
+            
+            for l in range(3, n+1):
+                for i in range(0, n-l+1):
+                    j = i+l-1
                     if s[i] == s[j]:
                         dp[i][j] = dp[i + 1][j - 1] + 2
                     else:
                         dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
-            
-            return not len(s)-dp[0][n-1] > k
+            return not (len(s) - dp[0][n-1] > k)
         
         return longestPalindromeSubseq(s)
             
