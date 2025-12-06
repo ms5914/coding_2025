9bd0329dc8b5506c2aa98dc9ecc8282a57d0afe3
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-05 23:35:49 -0800

    Time: 15 ms (96.32%), Space: 19 MB (91.79%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -9,3 +9,13 @@ class Solution:
             else:
                 return min(dfs(abs(x-1), abs(y-2)), dfs(abs(x-2), abs(y-1)))+1
         return dfs(abs(x),abs(y))
+
+
+        #Before explaining how we can apply the DFS algorithm to this problem, we should address the symmetry of the answers, which we haven't touched on so far.
+
+# We claim that the target (x,y), its horizontally, vertically, and diagonally symmetric points (i.e. (x,−y),(−x,y),(−x,−y)) share the same answer as the target point.
+
+# Due to the symmetry of the board (i.e. from -infinity to +infinity) and the symmetry of the allowed movements, we can rest assured that the above claim is correct, without rigid mathematical proof.
+
+# Based on the above insight, we can focus on the first quadrant of the coordinate plane where both x and y are positive.
+# Any target that is outside of the first quadrant, can be shifted to its symmetric point in the first quadrant by taking the absolute value of each coordinate, i.e. (∣x∣,∣y∣).
