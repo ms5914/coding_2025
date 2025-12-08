38cb9399a14265e4855177bce1cbe3d5c793282a
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-07 18:39:04 -0800

    Time: 1 ms (76.99%), Space: 18.6 MB (5.17%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,25 +1,30 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        word_set = set(wordDict)
 
-        self.nested_substring(s)
-        string = s
-        @lru_cache(maxsize=None)
-        def can_break(start):
-            if start == len(string):
-                return True
-            for end in range(start+1, len(string)+1):
-                prefix = self.nested.get((start, end), None)
-                if prefix in word_set:
-                    if can_break(end):
-                        return True 
-            return False
-        return can_break(0)
-
-    
-    def nested_substring(self, word):
-        self.nested = dict()
-        for i in range(0, len(word)):
-            for j in range(i+1, len(word)+1):
-                self.nested[(i,j)] = word[i:j]
+        class TrieNode:
+            def __init__(self):
+                self.children = dict()
+                self.is_word = False
         
+        root = TrieNode()
+        for word in wordDict:
+            curr = root
+            for c in word:
+                if not c in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.is_word = True
+
+        dp = [False]*len(s)
+        for i in range(len(s)):
+            if i == 0 or dp[i-1]:
+                curr = root
+                for j in range(i, len(s)):
+                    if not s[j] in curr.children:
+                        break
+                    curr = curr.children[s[j]]
+                    if curr.is_word:
+                        dp[j] = True
+        return dp[-1]
+        
+
