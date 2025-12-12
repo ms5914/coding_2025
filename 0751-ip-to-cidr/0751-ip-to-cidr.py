537fa9962f285ae1e98975b708263e28f7db8694
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-11 19:47:42 -0800

    Time: 2 ms (38.28%), Space: 17.6 MB (93.19%) - LeetHub

--- 0751-ip-to-cidr/0751-ip-to-cidr.py
+++ 0751-ip-to-cidr/0751-ip-to-cidr.py
@@ -1,22 +1,37 @@
 class Solution:
     def ipToCIDR(self, ip: str, n: int) -> List[str]:
-        def ip_to_bin(ip):
-            return "".join(["{:08b}".format(i) for i in map(int, ip.split("."))])
-        def bin_to_ip(b):
-            return ".".join([str(int(b[i:i+8],2)) for i in range(0, 32, 8)])
+        
+        def iptobin(ip):
+            return "".join(["{:08b}".format(int(number)) for number in ip.split(".")])
 
-
+        def bintoip(bin):
+            return ".".join([str(int(bin[i:i+8],2)) for i in range(0, 32, 8)])
+        
+        curr_ip = iptobin(ip)
         ans = []
-        cur_ip = ip_to_bin(ip)
-        while n > 0:
-            last_one = 31
-            while last_one> -1 and cur_ip[last_one] != '1':
-                last_one -= 1
-            trailing_zero = 31 - last_one
-            while n - (2 ** trailing_zero) < 0:
-                trailing_zero -= 1
-            ans.append(f"{bin_to_ip(cur_ip)}/{32 - trailing_zero}")
-            n -= 2 ** trailing_zero
-            cur_ip = "{:032b}".format(int(cur_ip, 2) + 2 ** trailing_zero)
+
+        while n:
+            i = len(curr_ip)-1
+            num_zeros = 0
+            
+            while i>=0 and curr_ip[i] == "0":
+                num_zeros+=1
+                i-=1
+            while pow(2, num_zeros) > n:
+                num_zeros-=1
+            
+            ans.append(bintoip(curr_ip)+"/"+str(32-num_zeros))
+            n-=pow(2, num_zeros)
+            curr_ip = "{:032b}".format(int(curr_ip, 2)+pow(2, num_zeros))
         return ans
+            
+            
+            
+            
+
+
+
+        
+
+
         
