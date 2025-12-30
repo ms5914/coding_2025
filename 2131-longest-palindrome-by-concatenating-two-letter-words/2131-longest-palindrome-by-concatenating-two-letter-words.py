41de36074ac70e7773fe079ee3ca30bc1ecf9655
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2025-12-30 15:46:08 -0800

    Time: 35 ms (98.47%), Space: 38 MB (7.29%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        count_words = Counter(words)
+        words_taken_count = 0
+        centre = False
+        for word, count in count_words.items():
+            if word[0] == word[1]:
+                if count%2 == 0:
+                    words_taken_count+=count
+                else:
+                    words_taken_count+=(count-1)
+                    centre = True
+            elif word[0]<word[1]:
+                words_taken_count+=2*min(count, count_words[word[1]+word[0]])
+        return words_taken_count*2 + 2 if centre else words_taken_count*2
+
+
+        
