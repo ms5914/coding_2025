3dafa2e5126944579276f888d25268f9f1bfb1fb
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2026-01-03 17:17:24 -0800

    Time: 0 ms (100%), Space: 17.3 MB (96.66%) - LeetHub

--- 0751-ip-to-cidr/0751-ip-to-cidr.py
+++ 0751-ip-to-cidr/0751-ip-to-cidr.py
@@ -1,30 +1,37 @@
 class Solution:
     def ipToCIDR(self, ip: str, n: int) -> List[str]:
-        
-        def iptobin(ip):
-            return "".join(["{:08b}".format(int(number)) for number in ip.split(".")])
 
-        def bintoip(bin):
-            return ".".join([str(int(bin[i:i+8],2)) for i in range(0, 32, 8)])
+        def ip_to_bin(ip):
+            binary_str = "".join(["{:08b}".format(int(segment)) for segment in ip.split(".")])
+            return binary_str
         
-        curr_ip = iptobin(ip)
-        ans = []
+        def bin_to_ip(binary):
+            ip = ".".join([str(int(binary[i:i+8], 2)) for i in range(0, 32, 8)])
+            return ip
 
-        while n:
-            i = len(curr_ip)-1
-            num_zeros = 0
+        result = []
+        current_ip = ip_to_bin(ip)
+        while n>0:
+            j = len(current_ip)-1
+            count_zeros = 0
+            while j>=0 and current_ip[j] == "0":
+                count_zeros+=1
+                j-=1
             
-            while i>=0 and curr_ip[i] == "0":
-                num_zeros+=1
-                i-=1
-            while pow(2, num_zeros) > n:
-                num_zeros-=1
+            while pow(2, count_zeros) > n:
+                count_zeros-=1
             
-            ans.append(bintoip(curr_ip)+"/"+str(32-num_zeros))
-            n-=pow(2, num_zeros)
-            curr_ip = "{:032b}".format(int(curr_ip, 2)+pow(2, num_zeros))
-        return ans
-            
+            result.append(bin_to_ip(current_ip)+"/"+str(32-count_zeros))
+            n-=pow(2, count_zeros)
+            current_ip = "{:032b}".format((int(current_ip, 2)+pow(2, count_zeros)))
+        
+        return result
+
+
+
+
+
+
             
             
             
