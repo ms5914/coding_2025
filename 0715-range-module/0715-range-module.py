edb71088e6a5ae7bc7d739a59356f705e5cbf3da
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2026-01-04 22:13:07 -0800

    Time: 55 ms (88.14%), Space: 22.3 MB (78.23%) - LeetHub

--- 0715-range-module/0715-range-module.py
+++ 0715-range-module/0715-range-module.py
@@ -1,16 +1,33 @@
+import bisect
 class RangeModule:
 
     def __init__(self):
+        self.arr = []
         
 
     def addRange(self, left: int, right: int) -> None:
+        l,r = bisect.bisect_left(self.arr, left), bisect.bisect_right(self.arr, right)
+        temp =[]
+        if l%2==0:
+            temp.append(left)
+        if r%2==0:
+            temp.append(right)
+        self.arr[l:r] = temp
         
 
     def queryRange(self, left: int, right: int) -> bool:
-        
+        l,r =  bisect.bisect_right(self.arr, left), bisect.bisect_left(self.arr, right)
+        return l==r and l%2==1
+
 
     def removeRange(self, left: int, right: int) -> None:
-        
+        l,r = bisect.bisect_left(self.arr, left), bisect.bisect_right(self.arr, right)
+        temp =[]
+        if l%2==1:
+            temp.append(left)
+        if r%2==1:
+            temp.append(right)
+        self.arr[l:r] = temp
 
 
 # Your RangeModule object will be instantiated and called as such:
