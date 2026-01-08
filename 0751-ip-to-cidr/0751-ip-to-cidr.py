30978ef1440e4b4579e97fff8d954d609c2c5a4f
Mahak Shah <55366357+ms5914@users.noreply.github.com>
2026-01-07 16:52:16 -0800

    Time: 3 ms (33.8%), Space: 19.5 MB (6.58%) - LeetHub

--- 0751-ip-to-cidr/0751-ip-to-cidr.py
+++ 0751-ip-to-cidr/0751-ip-to-cidr.py
@@ -6,35 +6,32 @@ class Solution:
             return ".".join(str(int(bin[i:i+8], 2)) for i in range(0, 32, 8))
 
         result = []
-        current_ip = ip_to_bin(ip)
+        ip_bin = ip_to_bin(ip)
         while n>0:
-            j = len(current_ip)-1
+            print(n)
+            i = len(ip_bin)-1
             count_zeros = 0
-            while j>=0 and current_ip[j] == "0":
+            
+            while i>=0 and ip_bin[i] == "0":
                 count_zeros+=1
-                j-=1
+                i-=1
             
             while pow(2, count_zeros) > n:
                 count_zeros-=1
             
-            result.append(bin_to_ip(current_ip)+"/"+str(32-count_zeros))
+            result.append(bin_to_ip(ip_bin)+"/"+str(32-count_zeros))
             n-=pow(2, count_zeros)
-            current_ip = "{:032b}".format((int(current_ip, 2)+pow(2, count_zeros)))
+            ip_bin = "{:032b}".format(int(ip_bin, 2)+pow(2, count_zeros))
         
         return result
 
+                
+                    
+                    
 
 
 
+                
 
 
-            
-            
-            
-
-
-
-        
-
-
-        
+                
